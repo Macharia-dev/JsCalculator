f48a39370eb55bdbfe884b9c40ae25a23942e170
David Macharia <169084424+Macharia-dev@users.noreply.github.com>
2024-09-28 11:34:53 +0300

    Add files via upload

---
+++ javascriptCalc/script.js
@@ -0,0 +1,22 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    document.getElementById('display').value += character;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        alert('Invalid operation');
+        clearDisplay();
+    }
+}
